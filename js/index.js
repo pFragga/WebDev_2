509099c2dd4a6e6bb820953d55f85b6078c984af
Peter Frangatzis <pfraggatzis2002@gmail.com>
2023-12-14 13:01:11 +0200

    http GET requests now mostly working

--- js/index.js
+++ js/index.js
@@ -1,60 +1,57 @@
-/* sample object returned from 'GET /categories' */
-let categoriesJson = `
-{
-  "categories": [
-    {
-      "id": 1,
-      "title": "Vehicles",
-      "img_url": "resources/αυτοκίνητα.jpg"
-    },
-    {
-      "id": 2,
-      "title": "Houses",
-      "img_url": "resources/ακίνητα.jpg"
+var adCategories = [];
+var adSubcategoriesById = [];
+var subcategoriesById = [
+  [
+    { id: 1, category_id: 1, title: 'alsdjkfl;asdfh' },
+    { id: 2, category_id: 1, title: 'z,mcbx,mbzcv' },
+  ],
+  [
+    { id: 1, category_id: 2, title: 'yqwoeriw' },
+    { id: 2, category_id: 2, title: 'The quick brown fox jumped over the lazy dogs.' },
+  ],
+  [
+    { id: 1, category_id: 3, title: 'Marcus Aurelius' },
+    { id: 2, category_id: 3, title: 'Biggus Dickus' },
+  ],
+];
+
+async function fetchStuff() {
+  let res = await fetch('https://wiki-ads.onrender.com/categories');
+  let data = await res.json();
+  adCategories = data;
+
+  if (adCategories != null) {
+    for (let category in adCategories) {
+      let res = await fetch(`https://wiki-ads.onrender.com/categories/:${category.id}/subcategories`);
+      let data = await res.json();
+      adSubcategoriesById.push(data);
     }
-  ]
+  }
 }
-`;
 
-/* sample object returned from 'GET /subcategories' */
-let subcategoriesJson = `
-{
-  "subcategories": [
-    {
-      "id": 1,
-      "category_id": 1,
-      "title": "Cars"
-    },
-    {
-      "id": 2,
-      "category_id": 1,
-      "title": "Motorbikes"
-    }
-  ]
-}
-`;
+window.onload = () => {
+  fetchStuff()
+    .then(() => {
+      /* add subcategories to categories list */
+      adCategories.forEach((category) => {
+        subcategoriesById.forEach((entry) => {
+          if (entry[0].category_id == category.id) {
+            category['subcategories'] = entry;
+          }
+        });
+      });
 
-window.addEventListener('load', function() {
-  let categoriesTag = document.getElementById('categories');
+      /* modify template */
+      let categoriesTag = document.getElementById('ad-categories');
+      let templateText = document.getElementById('template').textContent;
+      let templateFunc = Handlebars.compile(templateText);
+      let htmlContent = templateFunc({
+        data: adCategories
+      });
 
-  let categoriesTemplate = `
-  {{#each categories}}
-  <section class="category">
-  <h2>{{title}}</h2>
-  <a href="category.html?id={{id}}">
-    <img src="{{img_url}}" alt="Image that links to category page.">
-  </a>
-  <ul class="subcategories">
-  </ul>
-  </section>
-  {{/each}}
-  `;
-
-  let templateFunc = Handlebars.compile(categoriesTemplate);
-  let htmlContent = templateFunc({
-    categories: JSON.parse(categoriesJson).categories,
-    subcategories: JSON.parse(subcategoriesJson).subcategories,
-  });
-
-  categoriesTag.innerHTML = htmlContent;
-});
+      categoriesTag.innerHTML = htmlContent;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
