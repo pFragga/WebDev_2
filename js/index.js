465e3d98c78a7f5fed17777937e8b3bd10f274bf
Peter Frangatzis <pfraggatzis2002@gmail.com>
2023-12-15 14:38:52 +0200

    done: http requests working

--- js/index.js
+++ js/index.js
@@ -1,57 +1,52 @@
 var adCategories = [];
 var adSubcategoriesById = [];
-var subcategoriesById = [
-  [
-    { id: 1, category_id: 1, title: 'alsdjkfl;asdfh' },
-    { id: 2, category_id: 1, title: 'z,mcbx,mbzcv' },
-  ],
-  [
-    { id: 1, category_id: 2, title: 'yqwoeriw' },
-    { id: 2, category_id: 2, title: 'The quick brown fox jumped over the lazy dogs.' },
-  ],
-  [
-    { id: 1, category_id: 3, title: 'Marcus Aurelius' },
-    { id: 2, category_id: 3, title: 'Biggus Dickus' },
-  ],
-];
 
-async function fetchStuff() {
-  let res = await fetch('https://wiki-ads.onrender.com/categories');
-  let data = await res.json();
-  adCategories = data;
+async function fetchData(url, callback) {
+  let myHeaders = new Headers();
+  myHeaders.append('Accept', 'application/json');
 
-  if (adCategories != null) {
-    for (let category in adCategories) {
-      let res = await fetch(`https://wiki-ads.onrender.com/categories/:${category.id}/subcategories`);
-      let data = await res.json();
-      adSubcategoriesById.push(data);
+  let response = await fetch(
+    url,
+    {
+      method: 'GET',
+      headers: myHeaders
     }
-  }
+  );
+  let data = await response.json();
+  callback(data);
 }
 
-window.onload = () => {
-  fetchStuff()
-    .then(() => {
-      /* add subcategories to categories list */
-      adCategories.forEach((category) => {
-        subcategoriesById.forEach((entry) => {
-          if (entry[0].category_id == category.id) {
-            category['subcategories'] = entry;
-          }
-        });
+window.onload = async () => {
+  await fetchData('https://wiki-ads.onrender.com/categories', (data) => {
+    adCategories = data;
+  })
+  .then(async () => {
+    for (let category of adCategories) {
+      let id = category.id;
+      await fetchData(`https://wiki-ads.onrender.com/categories/${id}/subcategories`, (data) => {
+        adSubcategoriesById.push(data);
       });
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  })
+  .finally(() => {
+    /* add a subcategories field to each category */
+    for (let i = 0; i < adCategories.length; i++) {
+      for (let j = 0; j < adSubcategoriesById.length; j++) {
+        adCategories[i].subcategories = adSubcategoriesById[i];
+      }
+    }
 
-      /* modify template */
-      let categoriesTag = document.getElementById('ad-categories');
-      let templateText = document.getElementById('template').textContent;
-      let templateFunc = Handlebars.compile(templateText);
-      let htmlContent = templateFunc({
-        data: adCategories
-      });
-
-      categoriesTag.innerHTML = htmlContent;
-    })
-    .catch((error) => {
-      console.error(error);
+    /* modify template */
+    let categoriesTag = document.getElementById('ad-categories');
+    let templateText = document.getElementById('template').textContent;
+    let templateFunc = Handlebars.compile(templateText);
+    let htmlContent = templateFunc({
+      data: adCategories
     });
-};
+
+    categoriesTag.innerHTML = htmlContent;
+  });
+}
